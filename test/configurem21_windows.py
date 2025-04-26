84b2a1d7389c2cd9ce6124c9f55647fd8c5586dd
unknown <1463567152@qq.com>
2025-04-26 15:49:59 +0800

    github action windows 配置musescore路径

---
+++ test/configurem21_windows.py
@@ -0,0 +1,5 @@
+import music21
+
+us = music21.environment.UserSettings()
+us['musicxmlPath'] = 'C:\\Program Files\\MuseScore 3\\bin\\MuseScore3.exe'
+us['musescoreDirectPNGPath'] = 'C:\\Program Files\\MuseScore 3\\bin\\MuseScore4.exe'
